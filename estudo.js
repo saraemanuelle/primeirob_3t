fbde5fd701e400af860f2eb03ea5fec1a35a9520
saraemanuelle <112174263+saraemanuelle@users.noreply.github.com>
2022-09-30 09:06:30 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,19 @@
+tabuada = 6
+function escreva(){
+    document.writh("<h3> Tabuada do " + tabuada + "</h3>");
+   document.writhe(tabuada + "x 1 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 2 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 3 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 4 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 5 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 6 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 7 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 8 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 9 =" + (tabuada*1)+ "<abr>");
+   document.writhe(tabuada + "x 10 =" + (tabuada*1)+ "<abr>");
+   
+
+
+}
+
 
