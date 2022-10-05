6e9f77bc73991969ea4fa7095a12c1a2a52f7bb4
saraemanuelle <112174263+saraemanuelle@users.noreply.github.com>
2022-10-05 12:47:55 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,19 +1,35 @@
-tabuada = 6
+let tabuada = 111;
 function escreva(){
-    document.writh("<h3> Tabuada do " + tabuada + "</h3>");
-   document.writhe(tabuada + "x 1 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 2 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 3 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 4 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 5 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 6 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 7 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 8 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 9 =" + (tabuada*1)+ "<abr>");
-   document.writhe(tabuada + "x 10 =" + (tabuada*1)+ "<abr>");
-   
+    document.write("Tabuada do " + tabuada + "<br>");
+    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
+    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
+    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
+    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
+    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
+    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
+    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
+    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
+    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
+    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+}
 
+let lista = ["Jesus","Spessato","Duarte","Jorge","Neuza","Gilmar","Jossyara"];
 
+function mostraLista(){
+    document.write("Tamanho da lista: " + lista.length + "<br>");
+    for(let i = 0; i < lista.length; i++){
+        document.write("Professor: " + lista[i] + "<br>");
+    }
+}
+
+function multiplica(){
+    for(let i = 6; i <= 8; i++){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x "+j+" = " + (i*j) + "<br>");
+        }
+        document.write("<br>");
+    }
 }
 
 
